f1482f8c4bbb789288c3238e76464c05e6ad86ed
José A. Hernández Rodríguez <joseantoniohr87@gmail.com>
2020-06-15 16:37:30 +0100

    Create layout for tablets

--- app/src/main/java/com/timplesoft/appofthrones/CharactersActivity.kt
+++ app/src/main/java/com/timplesoft/appofthrones/CharactersActivity.kt
@@ -2,7 +2,6 @@ package com.timplesoft.appofthrones
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
